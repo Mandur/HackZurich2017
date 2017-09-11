a28960465e313bfcc3f1c88ff4b775e88ab771e3
Jonathan Karlsson <jonathan.karlsson@abb.com>
2017-09-11 16:32:44 +0200

    Add C# code example

---
+++ ABB/Examples/RemoteRobot/README.md
@@ -0,0 +1,10 @@
+# RemoteRobot
+Remote Control an ABB Robot
+
+## Usage notes
+1. Download the contents of this repository.
+2. Edit example Program.cs with url to robot.
+3. Use RobotStudio to open P&G.
+4. Start simulation from RobotStudio.
+5. Run example program.
+
---
+++ ABB/Examples/RemoteRobot/RemoteControlRobot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RemoteRobotLib;
+
+namespace RemoteControlRobot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string url = "192.168.54.108";
+            string url = "localhost";
+            var rightArm = new RemoteRobot(url, "T_ROB_R", RobotClientProvider.GetHttpClient(url));
+            var leftArm = new RemoteRobot(url, "T_ROB_L", RobotClientProvider.GetHttpClient(url));
+
+            Task.WhenAll(
+                rightArm.RunProcedure("wavehigh"),
+                leftArm.RunProcedure("wavelow"))
+                .Wait();
+            Console.WriteLine("Flip");
+            Task.Delay(2000).Wait();
+            Task.WhenAll(
+                rightArm.RunProcedure("wavelow"),
+                leftArm.RunProcedure("wavehigh"))
+                .Wait();
+
+            Console.WriteLine("Done");
+
+            Console.ReadKey();
+        }
+    }
+}
