40efead174a4c52fc84ed2c3e29c8af154b2ae8b
Mikhail Chatillon <email@example.com>
2017-09-13 20:59:40 +0200

    added deployment scripts

---
+++ Microsoft/RobotStudio-VM/SetUpVm.ps1
@@ -0,0 +1,21 @@
+﻿try
+{
+  #Verify if PowerShellGet module is installed. If not install
+  if (!(Get-Module -Name PowerShellGet))
+  {
+      Invoke-WebRequest 'http://cdn.robotstudio.com/install/RobotStudio_6.05.02.zip' -OutFile $($env:temp +'\RobotStudio_6.05.02.zip')
+      expand-archive -path $($env:temp +'\RobotStudio_6.05.02.zip') -destinationpath $($env:temp +'\RobotStudio')
+      Start-Process $($env:temp +'\RobotStudio\RobotStudio\ABB RobotStudio 6.05.02.msi') -ArgumentList "/qn" -Wait
+      netsh advfirewall firewall add rule name="Open Port 80" dir=in action=allow protocol=TCP localport=80
+  }
+    
+  #Verify if PSWindowsUpdate PowerShell Module is installed. If not install.
+    if (!(Get-Module -Name PSWindowsUpdate -List)){
+        Install-Module -Name PSWindowsUpdate -Scope AllUsers -Confirm:$false -Force
+    }
+    Get-WUInstall -WindowsUpdate -AcceptAll -AutoReboot -Confirm:$FALSE -ErrorAction stop
+}
+catch
+{
+    Write-Output "Oops. Something failed"
+}
