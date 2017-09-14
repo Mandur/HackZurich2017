e088588d0481e9aa8de2929dbef96364a9a9c546
Mikhail Chatillon <email@example.com>
2017-09-14 14:19:24 +0200

    cleaned JS folder

--- Microsoft/RobotStudio-VM/SetUpVm.ps1
+++ Microsoft/RobotStudio-VM/SetUpVm.ps1
@@ -4,13 +4,16 @@
   if (!(Get-Module -Name PowerShellGet))
   {
 
-      Invoke-WebRequest 'http://cdn.robotstudio.com/install/RobotStudio_6.05.02.zip' -OutFile $($env:temp +'\RobotStudio_6.05.02.zip')
-      expand-archive -path $($env:temp +'\RobotStudio_6.05.02.zip') -destinationpath $($env:temp +'\RobotStudio')
-      Start-Process $($env:temp +'\RobotStudio\RobotStudio\ABB RobotStudio 6.05.02.msi') -ArgumentList "/qn" -Wait
-      netsh advfirewall firewall add rule name="Open Port 80" dir=in action=allow protocol=TCP localport=80
+    
+         netsh advfirewall firewall add rule name="Open Port 80" dir=in action=allow protocol=TCP localport=80
       Invoke-WebRequest 'https://raw.githubusercontent.com/Mandur/HackZurich2017/master/ABB/RobotStudio/HackZurich.rspag' -OutFile $([Environment]::GetFolderPath("Desktop") +'\HackZurich.rspag')
       Invoke-WebRequest 'https://raw.githubusercontent.com/Mandur/HackZurich2017/master/Microsoft/RobotStudio-VM/vcconf.xml' -OutFile $([Environment]::GetFolderPath("Desktop") +'\vcconf.xml') 
   
+    Invoke-WebRequest 'http://cdn.robotstudio.com/install/RobotStudio_6.05.02.zip' -OutFile $($env:temp +'\RobotStudio_6.05.02.zip')
+      expand-archive -path $($env:temp +'\RobotStudio_6.05.02.zip') -destinationpath $($env:temp +'\RobotStudio')
+      Start-Process msiexec.exe -Wait -ArgumentList '/a '+$env:temp+'\RobotStudio\RobotStudio\ABB RobotStudio 6.05.02.msi'+' /quiet'
+      Start-Process $( +'') -ArgumentList "/qn /a" -Wait
+   
   }
     
   #Verify if PSWindowsUpdate PowerShell Module is installed. If not install.
