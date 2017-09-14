51eb81ba383238183d6577942e12685a07ba7213
Mikhail Chatillon <email@example.com>
2017-09-14 11:16:57 +0200

    Fixed code endpoint

--- Microsoft/RobotStudio-VM/SetUpVm.ps1
+++ Microsoft/RobotStudio-VM/SetUpVm.ps1
@@ -7,6 +7,8 @@
       expand-archive -path $($env:temp +'\RobotStudio_6.05.02.zip') -destinationpath $($env:temp +'\RobotStudio')
       Start-Process $($env:temp +'\RobotStudio\RobotStudio\ABB RobotStudio 6.05.02.msi') -ArgumentList "/qn" -Wait
       netsh advfirewall firewall add rule name="Open Port 80" dir=in action=allow protocol=TCP localport=80
+      #Invoke-WebRequest 'https://raw.githubusercontent.com/Mandur/HackZurich2017/master/Microsoft/RobotStudio-VM/HackZurich.rspag' -OutFile $($env:temp +'\RobotStudio_6.05.02.zip')
+  
   }
     
   #Verify if PSWindowsUpdate PowerShell Module is installed. If not install.
