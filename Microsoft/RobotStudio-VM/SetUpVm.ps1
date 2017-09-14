41a1b6c20dae662e4fff8e32c20f5b9a3934b75f
Mikhail Chatillon <email@example.com>
2017-09-14 21:12:33 +0200

    hotfix

--- Microsoft/RobotStudio-VM/SetUpVm.ps1
+++ Microsoft/RobotStudio-VM/SetUpVm.ps1
@@ -1,22 +1,4 @@
-﻿
-param (
-  $vmAdminUsername,
-  $vmAdminPassword
-)
- 
-$password =  ConvertTo-SecureString $vmAdminPassword -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential("$env:USERDOMAIN\$vmAdminUsername", $password)
- 
-Write-Verbose -Verbose "Entering Custom Script Extension..."
- 
-Invoke-Command -Credential $credential -ComputerName $env:COMPUTERNAME -ArgumentList $PSScriptRoot -ScriptBlock {
-  param 
-  (
-    $workingDir
-  )
- 
-
-try
+﻿try
 {
   #Verify if PowerShellGet module is installed. If not install
   if (!(Get-Module -Name PowerShellGet))
@@ -24,26 +6,30 @@ try
 
       netsh advfirewall firewall add rule name="Open Port 80" dir=in action=allow protocol=TCP localport=80
       New-Item $($env:TEMP  +'\HackZurich') -type directory
-      Invoke-WebRequest 'https://raw.githubusercontent.com/Mandur/HackZurich2017/master/ABB/RobotStudio/HackZurich.rspag' -OutFile $($env:TEMP  +'\HackZurich\HackZurich.rspag')
-      Invoke-WebRequest 'https://raw.githubusercontent.com/Mandur/HackZurich2017/master/Microsoft/RobotStudio-VM/vcconf.xml' -OutFile $($env:TEMP+'\HackZurich\vcconf.xml') 
+      Invoke-WebRequest 'https://raw.githubusercontent.com/Mandur/HackZurich2017/master/ABB/RobotStudio/HackZurich.rspag' -OutFile $([Environment]::GetFolderPath("Desktop") +'\HackZurich\HackZurich.rspag')
+      Invoke-WebRequest 'https://raw.githubusercontent.com/Mandur/HackZurich2017/master/Microsoft/RobotStudio-VM/vcconf.xml' -OutFile $([Environment]::GetFolderPath("Desktop")+'\HackZurich\vcconf.xml') 
       Add-WindowsCapability –Online -Name NetFx3~~~~ –Source D:\sources\sxs
       Invoke-WebRequest 'http://cdn.robotstudio.com/install/RobotStudio_6.05.02.zip' -OutFile $($env:TEMP +'\HackZurich\RobotStudio_6.05.02.zip')
       expand-archive -path $($env:TEMP +'\HackZurich\RobotStudio_6.05.02.zip') -destinationpath $($env:TEMP+'\HackZurich')
       Start-Process $($env:TEMP +'\HackZurich\RobotStudio\ABB RobotStudio 6.05.02.msi') -ArgumentList "/qn" -Wait
       cd $($env:TEMP+'\HackZurich\RobotStudio\')
-      .\setup.exe /s /v"/qb ADDLOCAL=ALL LICENSE_SERVER=myhostname"
+      .\setup.exe /s /v"/qb ADDLOCAL=ALL"
 
         
   }
+
+}
+catch
+{
+    Write-Output "Oops. Something failed"
+}
+
+try{
     
   #Verify if PSWindowsUpdate PowerShell Module is installed. If not install.
     if (!(Get-Module -Name PSWindowsUpdate -List)){
         Install-Module -Name PSWindowsUpdate -Scope AllUsers -Confirm:$false -Force
     }
     Get-WUInstall -WindowsUpdate -AcceptAll -AutoReboot -Confirm:$FALSE -ErrorAction stop
-}
-catch
-{
-    Write-Output "Oops. Something failed"
-}
+
 }
