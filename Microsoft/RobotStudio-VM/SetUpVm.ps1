363b2c6a2fc5f7aa406916ce06ea91ea29eaf0f1
Mikhail Chatillon <email@example.com>
2017-09-15 08:19:22 +0200

    one more deployment fix

--- Microsoft/RobotStudio-VM/SetUpVm.ps1
+++ Microsoft/RobotStudio-VM/SetUpVm.ps1
@@ -12,7 +12,6 @@
 
       Invoke-WebRequest 'http://cdn.robotstudio.com/install/RobotStudio_6.05.02.zip' -OutFile $($env:TEMP +'\HackZurich\RobotStudio_6.05.02.zip')
       expand-archive -path $($env:TEMP +'\HackZurich\RobotStudio_6.05.02.zip') -destinationpath $($env:TEMP+'\HackZurich')
-      Start-Process $($env:TEMP +'\HackZurich\RobotStudio\ABB RobotStudio 6.05.02.msi') -ArgumentList "/qn" -Wait
       cd $($env:TEMP+'\HackZurich\RobotStudio\')
       .\setup.exe /s /v"/qb ADDLOCAL=ALL LICENSE_SERVER=myhostname"
 
